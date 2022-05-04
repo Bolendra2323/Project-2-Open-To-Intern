ccb71ead6b9cd604d8919b4b4ed230ef836040c6
Bolendra -- Uranium <bolendra10@gmail.com>
2022-05-04 13:52:35 +0530

    Bolendraintern

---
+++ src/models/internModel.js
@@ -0,0 +1,33 @@
+const mongoose = requie('mongoose');
+
+const internSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        lowerCase: true,
+        type: String,
+        required: true,
+        unique: true,
+
+    },
+    mobile: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    collegeId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'colleges',
+        required: true,
+    },
+
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+module.exports = mongoose.model('intern', internSchema);
