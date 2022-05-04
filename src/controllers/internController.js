ed0e74d5aa510fda76a3f8ea2b010749d694a2d6
AbiMangalan <86941986+AbiMangalan@users.noreply.github.com>
2022-05-04 18:16:42 +0530

    Abi M -  internController

--- src/controllers/internController.js
+++ src/controllers/internController.js
@@ -3,52 +3,55 @@ const internModel = require("../models/internModel");
 const validators = require('../validators/validator');
 
 
-const createIntern = async function(req, res) {
-    try {
+const createIntern = async function(req, res) 
+{
+    try 
+    {
         let requestBody = req.body
 
-        if (!isValidRequestBody(requestBody)) {
-            res.status(400).send({ status: false, message: "please Provide Intern Details" })
+        if (!validators.isValidRequestBody(requestBody)) {
+            return res.status(400).send({ status: false, message: "please Provide Intern Details" })
         }
 
 
-        if (!isValidField(requestBody.name)) {
-            res.status(400).send({ status: false, message: "Name is Required" })
-        }
-
-
-
-        if (!isValidField(requestBody.email)) {
-            res.status(400).send({ status: false, message: "Email is Required" })
+        if (!validators.isValidField(requestBody.name)) {
+            return res.status(400).send({ status: false, message: "Name is Required" })
         }
 
 
 
-        if (!isValidField(requestBody.mobile)) {
-            res.status(400).send({ status: false, message: "Mobile Number is Required" })
+        if (!validators.isValidField(requestBody.email)) {
+            return res.status(400).send({ status: false, message: "Email is Required" })
         }
 
 
-        if (!isValidEmail(requestBody.email)) {
-            res.status(400).send({ status: false, message: "Enter Valid email Id" })
-        }
 
-        if (!isValidField(requestBody.name.college)) {
-            res.status(400).send({ status: false, message: "Name is Required" })
+        if (!validators.isValidField(requestBody.mobile)) {
+            return res.status(400).send({ status: false, message: "Mobile Number is Required" })
         }
 
 
-        if (!isValidObjectId(requestBody.collegeId)) {
-            res.status(400).send({ status: false, message: "Valid ID is required" })
+        if (!validators.isValidEmail(requestBody.email)) {
+            return res.status(400).send({ status: false, message: "Enter Valid email Id" })
+        }
+
+        if (!validators.isValidField(requestBody.collegeName)) {
+            return res.status(400).send({ status: false, message: "collegeName is Required" })
+        }
+
+        let college = await collegeModel.findOne({ name: requestBody.collegeName }, { _id: 1 })
+        if (college == null) {
+            return res.status(400).send({ status: false, message: "college not found" })
         }
 
 
-        var val = number.value
+
+        var val = requestBody.mobile
         if (/^\d{10}$/.test(val)) {
             // value is ok, use it
         } else {
             alert("Invalid number; must be ten digits")
-            number.focus()
+            val.focus()
             return res.status(400).send({ status: false, message: "Enter Valid Mobile Number" })
         }
         // d means "digit,"
@@ -56,18 +59,12 @@ const createIntern = async function(req, res) {
         // means "ten times."
         // The ^ and $ anchor it to the start and end, so something like asdf1234567890asdf does not match.
 
-        const collegeId = await collegeModel.findOne({ name: requestBody.name.college })
-        if (!collegeId) {
-            return res.status(400).send({ status: false, message: "College Name not found" })
-        }
-
-        if (collegeId.isDeleted === true) {
-            return res.status(400).send({ status: false, message: "Invalid field in request body" })
-        }
-
+        requestBody['collegeId']=college._id;
         const createIntern = await internModel.create(requestBody)
-        return res.status(201).send({ status: true, message: "Created" })
-    } catch (err) {
+        return res.status(201).send({ status: true, message: "Created",data : createIntern })
+    } 
+    catch (err) 
+    {
         res.status(500).send({ status: false, data: createIntern, message: err.message });
     }
 };
