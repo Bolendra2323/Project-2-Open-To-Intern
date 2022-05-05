ffedd2e28bc73d90a15a7efd1ae81c3a89f939d2
AbiMangalan <86941986+AbiMangalan@users.noreply.github.com>
2022-05-05 18:57:35 +0530

    Abi M - models

--- src/models/internModel.js
+++ src/models/internModel.js
@@ -1,32 +1,33 @@
 const mongoose = require('mongoose');
 
 const internSchema = new mongoose.Schema(
-{
-    name :  {
-                type: String,
-                required: true,
-                trim: true
-            },
-    email : {
-                type: String,
-                required: true,
-                unique: true,
-                lowerCase: true
-            },
-    mobile : {
-                type: String,
-                required: true,
-                unique: true
-             },
-    collegeId : {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: 'colleges',
-                    required: true
+    {
+        name :  {
+                    type: String,
+                    required: true,
+                    trim: true
                 },
-    isDeleted : {
-                    type: Boolean,
-                    default: false
-                }
-}, { timestamps: true });
+        email : {
+                    type: String,
+                    required: true,
+                    unique: true,
+                    lowerCase: true
+                },
+        mobile : {
+                    type: String,
+                    required: true,
+                    unique: true
+                },
+        collegeId : {
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: 'colleges',
+                        required: true
+                    },
+        isDeleted : {
+                        type: Boolean,
+                        default: false
+                    }
+    }
+);
 
 module.exports = mongoose.model('interns', internSchema);
